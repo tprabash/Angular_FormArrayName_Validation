97e84596d5724b9208a20f74a4a9f1a6b39b0705
tprabash <36395530+tprabash@users.noreply.github.com>
2023-01-04 12:46:12 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Angular_FormArrayName_Validation
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-reactive-form-with-nested-fields-and-vali-qzq5ue)
---
+++ app/app.component.ts
@@ -0,0 +1,63 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  companyForm: FormGroup;
+  isSubmitted: boolean;
+  titleAlert: string = 'This field is required';
+  post: any = '';
+  phoneRegex: RegExp = /^((3[0-9])|(6[0-9])|(8[1-9])|(9[0-8]))[0-9]{6}$/;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    // init form
+    this.createForm();
+  }
+
+  createForm() {
+    this.companyForm = this.formBuilder.group({
+      contacts: this.formBuilder.array([
+        this.formBuilder.group({
+          name: ['', Validators.required],
+          phone: [
+            '',
+            [Validators.required, Validators.pattern(this.phoneRegex)],
+          ],
+        }),
+      ]),
+    });
+  }
+
+  onSubmit(post, isValid) {
+    this.isSubmitted = true;
+
+    if (!isValid) return;
+
+    this.post = post;
+  }
+
+  // add new form group to contacts array
+  addContactField() {
+    let control = <FormArray>this.companyForm.controls.contacts;
+    control.push(
+      this.formBuilder.group({
+        name: ['', Validators.required],
+        phone: ['', [Validators.required, Validators.pattern(this.phoneRegex)]],
+      })
+    );
+  }
+
+  get emails() {
+    return this.companyForm.get('emails') as FormArray;
+  }
+
+  get contacts() {
+    return this.companyForm.get('contacts') as FormArray;
+  }
+}
